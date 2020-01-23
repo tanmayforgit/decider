746d56719bdb8040d6386e52b2196631be3a29cb
Tanmay Tupe <tanmaytupe@gmail.com>
2020-01-23 11:44:19 +0530

    Improved specs for workflow existance in master workflow.rb

--- spec/models/decider/master_workflow_spec.rb
+++ spec/models/decider/master_workflow_spec.rb
@@ -12,6 +12,17 @@ module Decider
           expect(subject).to be_falsey
         end
       end
+
+      context 'Operation configuration#workflow_names does have workflow name' do
+        let(:workflow_name) { 'abcd_pqrs' }
+        let(:master_workflow) { MasterWorkflow.new(workflow_name) }
+
+        subject { master_workflow.exists? }
+        it 'Returns false' do
+          allow(Decider::OperationConfiguration).to receive(:workflow_names) { [workflow_name.to_sym, :something_else] }
+          expect(subject).to be_truthy
+        end
+      end
     end
   end
 end
